79a1724f0330514a16f81b4250b8835b25f59f27
Giannuz <36342018+Giannuz@users.noreply.github.com>
2024-04-07 17:08:26 +0200

    script + apks

---
+++ kali-mobilesecsetup.sh
@@ -0,0 +1,33 @@
+sudo apt-get install jadx
+sudo apt-get install dex2jar
+sudo apt-install jd-gui
+sudo apt-get install jd-gui
+
+# to install mobsf we need docker
+sudo apt install docker.io
+docker pull opensecurity/mobile-security-framework-mobsf
+
+# run mobsf comment next line if not needed
+sudo docker run -it -p 8000:8000 opensecurity/mobile-security-framework-mobsf:latest
+
+
+# APKHunt section
+git clone https://github.com/Cyber-Buddy/APKHunt.git
+cd APKHunt
+sudo apt install golang-go
+
+
+# SEBASTiAn section
+cd ..
+git clone https://github.com/talos-security/SEBASTiAn
+cd SEBASTiAn && virtualenv -p python3 venv
+source venv/bin/activate
+python3 -m pip install -r src/requirements.txt
+
+
+
+
+
+# to add to alias 
+echo "alias mobsf='sudo docker run -it -p 8000:8000 opensecurity/mobile-security-framework-mobsf:latest'" >> ~/.zshrcgo 
+run apkhunt.go >> ~/.zshrcgo
