1ededcd50cd6d69e3ea93d5e3d55bca63afda551
Giannuz <36342018+Giannuz@users.noreply.github.com>
2024-05-10 09:46:02 +0200

    Update kali-mobilesecsetup.sh with new commands

--- kali-mobilesecsetup.sh
+++ kali-mobilesecsetup.sh
@@ -1,4 +1,4 @@
-sudo apt-get update
+sudo apt update
 
 sudo apt-get install jadx
 sudo apt-get install dex2jar
@@ -22,8 +22,43 @@ cd SEBASTiAn && virtualenv -p python3 venv
 source venv/bin/activate
 python3 -m pip install -r src/requirements.txt
 
+#charles
+wget -qO- https://www.charlesproxy.com/packages/apt/charles-repo.asc | sudo tee /etc/apt/keyrings/charles-repo.asc
+sudo sh -c 'echo deb [signed-by=/etc/apt/keyrings/charles-repo.asc] https://www.charlesproxy.com/packages/apt/ charles-proxy main > /etc/apt/sources.list.d/charles.list'
+sudo apt-get update && sudo apt-get install charles-proxy
+
+#  apk-mitm
+
+# installs NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+# download and install Node.js
+nvm install 20
+
+# verifies the right Node.js version is in the environment
+node -v # should print `v20.13.1`
+
+# verifies the right NPM version is in the environment
+npm -v # should print `10.5.2`
+
+sudo apt-get install default-jdk
+
+npm install -g apk-mitm
 
 
+# Frida
+sudo apt install python3-pip
+sudo pip3 install --upgrade frida-tools
+wget https://github.com/frida/frida/releases/download/16.2.1/frida-server-16.2.1-android-arm64.xz -P /home/michele/Downloads
+
+# Objection
+sudo pip3 install --upgrade objection
+
+
+# drozer
+wget https://github.com/WithSecureLabs/drozer/releases/download/3.0.2/drozer-3.0.2-py3-none-any.whl -P /home/michele/Downloads
+sudo apt install python3.11-venv
+pipx install /home/michele/Downloads/drozer-3.0.2-py3-none-any.whl
 
 
 # to add to alias 
